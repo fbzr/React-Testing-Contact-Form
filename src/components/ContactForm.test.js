2a0afb508f44f74cea6de6a3b06feca2b85b693a
fbzr <fabricio.spb@gmail.com>
2020-03-12 13:23:35 -0700

    create test to get elements from form

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -3,3 +3,9 @@ import { render, fireEvent } from '@testing-library/react';
 import ContactForm from './ContactForm';
 
 test('renders component', () => { render(<ContactForm />) });
+
+test('gets elements from form', () => {
+    const dom = render(<ContactForm />);
+
+    const emailInput = dom.getByLabelText(/email*/i);
+})
